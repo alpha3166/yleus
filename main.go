f0369d2eadaf333e138ec67e5213cac6c06aeb93
alpha3166 <foxtrot3166-git@yahoo.co.jp>
2023-05-04 05:03:43 +0000

    feat: Avoid picking headlines' summary

--- main.go
+++ main.go
@@ -47,7 +47,11 @@ func process(target string) {
 	article := article{}
 	article.fillDefaults()
 	doc.Find("article.yle__article").Find("h1, h2, h3, p").Each(func(i int, s *goquery.Selection) {
-		article.handle(goquery.NodeName(s), s.Text())
+		if goquery.NodeName(s) == "p" && goquery.NodeName(s.Parent()) == "figure" {
+			// ignore
+		} else {
+			article.handle(goquery.NodeName(s), s.Text())
+		}
 	})
 	article.close()
 }
